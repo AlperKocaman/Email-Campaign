5ba042879468e677040eaa17de527c8d8a8a2cc2
AlperKocaman <alperkman@gmail.com>
2021-08-10 00:15:24 +0300

    ApiController class initialized.

---
+++ src/main/java/com/picus/email_campaign/controller/ApiController.java
@@ -0,0 +1,16 @@
+package com.picus.email_campaign.controller;
+
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "api")
+@RequiredArgsConstructor
+@Slf4j
+@Data
+public class ApiController {
+}
