258c5d22e5894f357453aebffe55b3cdadb5f97f
AlperKocaman <alperkman@gmail.com>
2021-08-09 21:07:46 +0300

    Project initialized.

---
+++ src/main/java/com/picus/email_campaign/EmailCampaignApplication.java
@@ -0,0 +1,13 @@
+package com.picus.email_campaign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailCampaignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailCampaignApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/picus/email_campaign/EmailCampaignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.picus.email_campaign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailCampaignApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
